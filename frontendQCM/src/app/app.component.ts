4381600510e45580664a2e591c645a9cde636b71
Thomxsss <thomas.trognon@outlook.fr>
2021-11-11 14:20:31 +0100

    Version final de la top et de la side bar

--- frontendQCM/src/app/app.component.ts
+++ frontendQCM/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
-import {MatSidenav} from "@angular/material/sidenav";
-import {BreakpointObserver} from '@angular/cdk/layout'
+
 
 @Component({
   selector: 'app-root',
@@ -9,24 +8,10 @@ import {BreakpointObserver} from '@angular/cdk/layout'
 })
 export class AppComponent {
   title = 'frontendQCM';
-  //
-  // @ViewChild(MatSidenav)
-  // sidenav!:MatSidenav;
-  //
-  // private observer: BreakpointObserver
 
   constructor() {
   }
 
-  // ngAfterViewInit(){
-  //   this.observer.observe(['(max-width: 800px)']).subscribe((res)=>{
-  //     if (res.matches){
-  //       this.sidenav.mode='over';
-  //       this.sidenav.close();
-  //     } else {
-  //       this.sidenav.mode='side';
-  //       this.sidenav.open();
-  //     }
-  //   })
+
 
 }
--- frontendQCM/src/app/layout/layout.component.ts
+++ frontendQCM/src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 
 @Component({
@@ -9,16 +10,33 @@ import { Component, OnInit, Input } from '@angular/core';
 export class LayoutComponent implements OnInit {
 
   isOpen:boolean=true;
+  // @ts-ignore
 
-  constructor() { }
+  constructor(private observer: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
+
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 80rem)']).subscribe((res) => {
+      if (res.matches) {
+        this.isOpen=false;
+      } else {
+        this.isOpen=true;
+      }
+    })
+  }
+
 
   navOpen($event:any): void {
     // toggle condition here
     this.isOpen = !this.isOpen;
     console.log('$navOpen');
   }
-
 }
+
+
+
+
+
