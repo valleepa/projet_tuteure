03be5cd1c0255286bcb7686736b6683cb1db67fc
Vranox <damyscus@gmail.com>
2022-01-07 23:40:23 +0100

    ajout d'un objet qcm

--- src/app/Component/tableau/tableau.component.ts
+++ src/app/Component/tableau/tableau.component.ts
@@ -9,12 +9,17 @@ import {QCM} from "../../QCM";
 })
 export class TableauComponent implements OnInit {
   qcms: QCM[] = [];
+  qcm = <QCM>{};
   dataSource!: MatTableDataSource<QCM>;
   displayedColumns: string[] = ['name', 'modify', 'mark'];
   constructor() {
   }
 
   ngOnInit(): void {
+    this.qcm.name = "QCM1";
+    this.qcm.id = 1;
+    this.qcms.push(this.qcm);
+    this.dataSource = new MatTableDataSource<QCM>(this.qcms)
   }
   applyFilter(event: Event) {
     this.dataSource.filterPredicate = function(data, filter: string): boolean {
