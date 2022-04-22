b8fed9ceac74d59a80a9974b248d1f96fce8bcbe
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-04-22 09:32:31 +0200

    Init du component Connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { CreationQuestionComponent } from './Component/Creation/creation-questio
 import { CreationQuestionDefautComponent } from './Component/Creation/creation-question/creation-question-defaut/creation-question-defaut.component';
 import { CreationQuestionOuverteComponent } from './Component/Creation/creation-question/creation-question-ouverte/creation-question-ouverte.component';
 import { CreationQuestionNumeriqueComponent } from './Component/Creation/creation-question/creation-question-numerique/creation-question-numerique.component';
+import { ConnexionComponent } from './Component/connexion/connexion.component';
 
 
 // import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2";
@@ -73,6 +74,7 @@ import { CreationQuestionNumeriqueComponent } from './Component/Creation/creatio
     CreationQuestionDefautComponent,
     CreationQuestionOuverteComponent,
     CreationQuestionNumeriqueComponent,
+    ConnexionComponent,
   ],
   imports: [
     BrowserModule,
