80411dbbc897d14fab2d99d726485487565b032d
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-04-28 21:12:14 +0200

    Component de connexion

--- src/app/Component/Connexion/connexion.component.ts
+++ src/app/Component/Connexion/connexion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthenticationService} from "../../Services/authentication.service";
 
 @Component({
   selector: 'app-connexion',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connexion.component.scss']
 })
 export class ConnexionComponent implements OnInit {
+  username: string | undefined;
+  password : string | undefined;
+  errorMessage = 'Invalid Credentials';
+  successMessage: string | undefined;
+  invalidLogin = false;
+  loginSuccess = false;
 
-  constructor() { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService) {   }
 
   ngOnInit(): void {
   }
 
+  connexion() {
+    if (this.username != null && this.password != null) {
+      this.authenticationService.authenticationService(this.username, this.password).subscribe(r => {
+        this.invalidLogin = false;
+        this.loginSuccess = true;
+        this.successMessage = 'Login Successful.';
+        this.router.navigate(['']).then(() => window.location.reload());
+      }, () => {
+        this.invalidLogin = true;
+        this.loginSuccess = false;
+      });
+    }
+  }
 }
