bda3543b295694b42311638014a5a617bd66d5e9
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-04-28 21:12:54 +0200

    Création de l'intercepteur http

---
+++ src/app/Services/httpinterceptor.service.ts
@@ -0,0 +1,24 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable()
+export class HttpInterceptorService implements HttpInterceptor {
+
+  constructor(private authenticationService: AuthenticationService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let localHeaders = req.headers
+    if(this.authenticationService.isUserLoggedIn()){
+      localHeaders = localHeaders.set('Authorization',`Basic ${window.btoa(this.authenticationService.username + ":" + this.authenticationService.password)}`)
+    }
+    if(!req.headers.get('Content-Type')){
+      localHeaders = localHeaders.set('Content-Type','application/json')
+    }
+    req = req.clone({
+      headers:localHeaders
+    })
+    return next.handle(req);
+  }
+}
