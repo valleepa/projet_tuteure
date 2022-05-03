ceb849029066a7e1ba6a2a09450640074f17551b
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-05-03 21:07:55 +0200

    Création du modèle d'USER

---
+++ src/app/Modeles/USER.ts
@@ -0,0 +1,45 @@
+export interface IUser {
+  id : any,
+  username : string,
+  email: string,
+  nom : string,
+  prenom : string,
+  password : string,
+  isAdmin : boolean,
+  groupes : [],
+  enabled : boolean,
+  authorities : null,
+  accountNonExpired : boolean,
+  accountNonLocked : boolean,
+  credentialsNonExpired : boolean
+}
+
+export class User implements IUser {
+  public id : any;
+  public username : string
+  public email: string
+  public nom : string
+  public prenom : string
+  public password : string
+  public isAdmin : boolean
+  public groupes : []
+  public enabled : boolean = false
+  public authorities : null
+  public accountNonExpired : boolean = false
+  public accountNonLocked : boolean = false
+  public credentialsNonExpired : boolean = false
+
+  public constructor(username:string,email:string,nom:string,prenom:string,password:string,isAdmin:boolean,groupes:[]) {
+    this.username = username
+    this.email = email
+    this.nom = nom
+    this.prenom = prenom
+    this.password = password
+    this.isAdmin = isAdmin
+    this.groupes = groupes
+  }
+
+  getId() {
+    return this.id;
+  }
+}
