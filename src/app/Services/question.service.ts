3206b42372fe6fecfa585db26c23402eb59b533a
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-05-14 19:12:01 +0200

    Adaptation from pour le back

--- src/app/Services/question.service.ts
+++ src/app/Services/question.service.ts
@@ -8,8 +8,8 @@ import {QCM} from "../Modeles/QCM";
   providedIn: 'root'
 })
 export class QuestionService {
-  questionActuel = new BehaviorSubject(new Question('nullll', 'null', [],""));
-  categorieActuel = new BehaviorSubject(new Categorie('nu', [new Question('nul', 'null', [],"")]));
+  questionActuel = new BehaviorSubject(new Question('nullll', 'null', [],"",""));
+  categorieActuel = new BehaviorSubject(new Categorie('nu', [new Question('nul', 'null', [],"","")]));
   QCMActuel = new BehaviorSubject(new QCM([],'',0,false,'null','null'));
   constructor() {
     if(localStorage.getItem("QCM")){
