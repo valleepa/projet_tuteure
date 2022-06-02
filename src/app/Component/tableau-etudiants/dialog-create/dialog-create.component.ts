2d46ba53b10157d64c22455f63263a7043089338
Anthony <varmaanthony@gmail.com>
2022-06-02 20:47:42 +0200

    ajout du componenent dialog

---
+++ src/app/Component/tableau-etudiants/dialog-create/dialog-create.component.ts
@@ -0,0 +1,24 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-dialog-create',
+  templateUrl: './dialog-create.component.html',
+  styleUrls: ['./dialog-create.component.scss']
+})
+export class DialogCreateComponent implements OnInit {
+
+  // @ts-ignore
+  nom: any;
+  classe: any;
+  groupe: any;
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any) { }
+
+  ngOnInit(): void {
+  }
+
+  creer(){
+    console.log("nom : " + this.nom, "|| classe : " + this.classe, "|| groupe : " + this.groupe);
+
+  }
+}
