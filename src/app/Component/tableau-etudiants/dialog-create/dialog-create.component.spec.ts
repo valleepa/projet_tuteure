05d47fa313c871f87bc995d55511a054fbf5c76b
Anthony <varmaanthony@gmail.com>
2022-06-02 20:47:42 +0200

    ajout du componenent dialog

---
+++ src/app/Component/tableau-etudiants/dialog-create/dialog-create.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DialogCreateComponent } from './dialog-create.component';
+
+describe('DialogCreateComponent', () => {
+  let component: DialogCreateComponent;
+  let fixture: ComponentFixture<DialogCreateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DialogCreateComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DialogCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
