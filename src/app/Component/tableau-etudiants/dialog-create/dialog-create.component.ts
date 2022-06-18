73e8d765326a14060ff7358566e5f72b4f18d00c
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-06-18 17:29:25 +0200

    Mise en action de toutes les fonctionnalités de la boîte de dialogue de création des utilisateurs pour permettre leur création. Ajout des champs Prénom et nom étudiant

--- src/app/Component/tableau-etudiants/dialog-create/dialog-create.component.ts
+++ src/app/Component/tableau-etudiants/dialog-create/dialog-create.component.ts
@@ -1,5 +1,14 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {EtudiantsService} from "../../../Services/etudiants.service";
+import {ClasseService} from "../../../Services/classe.service";
+import {GroupeService} from "../../../groupe.service";
+import {Classe} from "../../../Modeles/CLASSE";
+import {Groupe} from "../../../Modeles/GROUPE";
+import {FormControl} from "@angular/forms";
+import {MatSelectChange} from "@angular/material/select";
+import {Etudiant} from "../../../Modeles/ETUDIANTS";
+
 
 @Component({
   selector: 'app-dialog-create',
@@ -12,9 +21,54 @@ export class DialogCreateComponent implements OnInit {
   nom: any;
   classe: any;
   groupe: any;
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any) { }
+  classes: Classe[] = [];
+  classesList: String[] = [];
+  groupes: Groupe[] = [];
+  groupesList: String[] = [];
+  classesForm = new FormControl('');
+  groupesForm = new FormControl('');
+
+  selectedClasse: Classe | null =  null;
+  id : String | null = sessionStorage.getItem('ID');
+  numetu: any;
+  prenom: any;
+  created = false;
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any, private etudiantsService:EtudiantsService,private classeService : ClasseService, private groupeService: GroupeService) { }
 
   ngOnInit(): void {
+    this.classeService.getClassesFromUser(<number><unknown>this.id).subscribe(r => {
+      this.classes = r;
+      for(let i = 0 ; i < r.length ; i++){
+        this.classesList.push(r[i].nomClasse)
+      }
+    })
   }
 
+  createEtudiant() {
+    this.etudiantsService.createEtudiant({"nom":this.nom,"prenom":this.prenom,"noetudiant":this.numetu,"classe":this.classe,"groupe":this.groupe}).subscribe(r=>{
+      this.created = (r != null);
+    })
+  }
+
+  onClasseChange($event: MatSelectChange) {
+    for(let i = 0 ; i < this.classes.length ; i++ ){
+      if(this.classes[i].nomClasse == $event.value){
+        this.selectedClasse = this.classes[i];
+      }
+    }
+    this.classe = $event.value;
+    if(this.selectedClasse != null){
+      this.groupeService.getGroupesFromUser(<number><unknown>this.id,this.selectedClasse.id).subscribe(r => {
+
+        this.groupes = r;
+        for(let i = 0 ; i < r.length ; i++){
+          this.groupesList.push(r[i].nomGroupe)
+        }
+      })
+    }
+  }
+
+  onGroupeChange($event: MatSelectChange) {
+    this.groupe = $event.value;
+  }
 }
