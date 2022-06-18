34ee0fcf8afcefc947cfb90a96b59c86af6948ee
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-06-18 17:26:12 +0200

    Résolution de l'ambiguité du numéro étudiant

--- src/app/Modeles/ETUDIANTS.ts
+++ src/app/Modeles/ETUDIANTS.ts
@@ -4,7 +4,7 @@ export interface IEtudiant {
   groupe: string;
   nom: string;
   prenom:string;
-  nOEtudiant:number;
+  noetudiant:number;
 }
 
 export class Etudiant implements IEtudiant {
@@ -13,13 +13,13 @@ export class Etudiant implements IEtudiant {
   public groupe: string;
   public nom: string;
   public prenom: string;
-  public nOEtudiant: number;
-  constructor(id:number, classe:string, groupe:string, nom:string, prenom:string, nOEtudiant:number) {
+  public noetudiant: number;
+  constructor(id:number, classe:string, groupe:string, nom:string, prenom:string, noetudiant:number) {
     this.id = id;
     this.classe =  classe;
     this.groupe = groupe;
     this.nom = nom;
     this.prenom = prenom;
-    this.nOEtudiant = nOEtudiant;
+    this.noetudiant = noetudiant;
   }
 }
