27a2d612d184dde6bc597f93852beec0ecf6b0d8
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-06-19 22:15:34 +0200

    Création du massimport depuis les CSV.

--- src/app/Component/tableau-etudiants/tableau-etudiants.component.ts
+++ src/app/Component/tableau-etudiants/tableau-etudiants.component.ts
@@ -6,19 +6,28 @@ import {EtudiantsService} from "../../Services/etudiants.service";
 import {MatDialog} from "@angular/material/dialog";
 import {DialogCreateComponent} from "./dialog-create/dialog-create.component";
 import {group} from "@angular/animations";
+import {GroupeService} from "../../groupe.service";
+import {Classe} from "../../Modeles/CLASSE";
+import {ClasseService} from "../../Services/classe.service";
+import {Groupe} from "../../Modeles/GROUPE";
 
 @Component({
   selector: 'app-tableau-etudiants',
   templateUrl: './tableau-etudiants.component.html',
   styleUrls: ['./tableau-etudiants.component.scss']
 })
+
 export class TableauEtudiantsComponent implements OnInit {
   id = sessionStorage.getItem("ID");
   etudiants: IEtudiant[] = [];
   dataSource!: MatTableDataSource<IEtudiant>;
+  classes:Classe[] = [];
+  groupes:Groupe[] = [];
+  classesAndGroupes = {} as IDictionary;
   displayedColumns: string[] = ['name','surname','numetu', 'class', 'group', 'profil'];
+  added: number = -1;
 
-  constructor(public dialog: MatDialog, private router: Router, private etudiantsService: EtudiantsService) { }
+  constructor(public dialog: MatDialog, private router: Router, private etudiantsService: EtudiantsService, private groupeService: GroupeService, private classeService:ClasseService) { }
 
   ngOnInit(): void {
     if(this.id != null){
@@ -29,6 +38,20 @@ export class TableauEtudiantsComponent implements OnInit {
         }
       });
     }
+
+    this.classeService.getClassesFromUser(Number(this.id)).subscribe(r=>{
+      this.classes = r;
+      this.classes.forEach(each=>{
+        this.groupeService.getGroupesFromUser(each.id,Number(this.id)).subscribe(r=>{
+          this.classesAndGroupes[each.nomClasse] = [];
+          r.forEach(eachg=>{
+            this.classesAndGroupes[each.nomClasse].push(eachg.nomGroupe);
+          })
+        })
+      })
+    })
+
+
   }
   showProfil(etudiant:Etudiant):void{
     this.etudiantsService.etudiantActuel = etudiant;
@@ -48,7 +71,63 @@ export class TableauEtudiantsComponent implements OnInit {
       data: { title: "Création d'un étudiant"}
     });
     dialogRef.afterClosed().subscribe(
-      result => console.log('Dialog Result', result)
+      result => {
+        if(result != ""){
+          this.etudiants.push(result);
+          this.dataSource.data = this.etudiants;
+        }
+      }
     );
   }
+
+  file:any;
+  fileChanged($event:any) {
+    this.file = $event.target.files[0]
+  }
+
+  uploadDocument(file:any) {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      (this.validateCSV(fileReader.result).then((value => {
+        this.added = value;
+      })));
+    }
+    fileReader.readAsText(file);
+  }
+
+  async validateCSV(csv: String | ArrayBuffer | null) {
+    if (typeof csv === 'string') {
+      let added = 0;
+      let lines: String[] = csv.split("\n");
+      for (let i = 0; i < lines.length; i++) {
+        let values = lines[i].split(";");
+        if (values.length == 5 && !isNaN(Number(values[2]))) {
+          const t = await this.etudiantsService.createEtudiant({
+            "nom": values[0],
+            "prenom": values[1],
+            "noetudiant": values[2],
+            "classe": values[3],
+            "groupe": values[4]
+          }).toPromise();
+          if(t != null){
+            added += 1;
+            this.etudiants.push(new Etudiant(t.id,t.classe,t.groupe,t.nom,t.prenom,t.noetudiant))
+            this.dataSource.data = this.etudiants;
+          }
+        }
+      }
+      return added;
+    }
+    return -1;
+  }
+
+  onCSVSubmit() {
+    if(this.file){
+      this.uploadDocument(this.file);
+    }
+  }
+}
+
+interface IDictionary {
+  [index: string]: string [];
 }
