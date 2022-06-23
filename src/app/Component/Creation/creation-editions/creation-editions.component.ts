5d42a4f6304afdb9492ef4dd82a877e9069f6c52
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-06-23 20:49:49 +0200

    Fin de l'interface de génération (conditionnement, ajout de la classe reçue, etc)

--- src/app/Component/Creation/creation-editions/creation-editions.component.ts
+++ src/app/Component/Creation/creation-editions/creation-editions.component.ts
@@ -5,6 +5,13 @@ import {Router} from "@angular/router";
 import {QCM} from "../../../Modeles/QCM";
 import {QcmService} from "../../../Services/qcm.service";
 import {QuestionService} from "../../../Services/question.service";
+import {EtudiantsService} from "../../../Services/etudiants.service";
+import {ClasseService} from "../../../Services/classe.service";
+import {GroupeService} from "../../../groupe.service";
+import {Groupe} from "../../../Modeles/GROUPE";
+import {Classe} from "../../../Modeles/CLASSE";
+import {MatSelectChange} from "@angular/material/select";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-creation-editions',
@@ -13,37 +20,108 @@ import {QuestionService} from "../../../Services/question.service";
 })
 export class CreationEditionsComponent implements OnInit {
   qcm : QCM | undefined;
-  constructor(public sanitizer:DomSanitizer, public questionService:QuestionService, public qcmService:QcmService) {
+  id : any = sessionStorage.getItem('ID');
+  groupes : Groupe[] = [];
+  classes : Classe[] = [];
+  selectedClasse : any = null;
+  classesList: String[] = [];
+  selectedGroupe : any = null;
+  groupesList: String[] = [];
+  classesForm = new FormControl('');
+  groupesForm = new FormControl('');
+  public errormessage: string = "";
+
+  constructor(public sanitizer:DomSanitizer, public questionService:QuestionService, public qcmService:QcmService, public classeService:ClasseService,public groupeService : GroupeService) {
+    if(this.id != null){
+      this.classeService.getClassesFromUser(<number>this.id).subscribe(r=>{
+        r.forEach(classe=>{
+          this.classes.push(classe);
+          this.classesList.push(classe.nomClasse)
+        })
+      })
+    }
+
+
   }
 
   pdf : SafeResourceUrl | null= null;
   isGenerate: boolean = false;
 
+
   ngOnInit(): void {
     this.questionService.QCMActuel.subscribe(r=>{
       this.qcm =r;
     });
-  }
-
-  generateQCM() {
-    if(this.qcm != undefined){
-      console.log("GENERATE")
-      this.qcmService.generateNewQCM(this.qcm).subscribe(r=>{
-        if(r != null){
+    this.qcmService.getPDFFromIdAndId(this.qcm?.idcreateur,this.qcm?.id).then(r=>{
+        if(r.size != 0){
+          let url = window.URL.createObjectURL(r);
+          this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
           this.isGenerate = true;
-          this.qcmService.getPDFFromIdAndId(this.qcm?.idcreateur,this.qcm?.id).then(r=>{
-              this.pdf = r;
-              let url = window.URL.createObjectURL(r);
-              this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-            }
-          )
         }
         else{
           this.isGenerate = false;
           this.pdf = null;
         }
-      });
+      }
+    )
+  }
+
+  generateQCM() {
+    let requ;
+    if (this.qcm != undefined) {
+      if (this.selectedClasse != undefined) {
+        if (this.selectedGroupe != undefined) {
+          requ = this.qcmService.generateNewQCM(this.qcm, this.selectedClasse.id, this.selectedGroupe.id);
+        }
+        else{
+          requ = this.qcmService.generateNewQCM(this.qcm, this.selectedClasse.id, null);
+        }
+          requ.subscribe(r => {
+          if (r != null) {
+            this.isGenerate = true;
+            this.qcmService.getPDFFromIdAndId(this.qcm?.idcreateur, this.qcm?.id).then(r => {
+                this.pdf = r;
+                let url = window.URL.createObjectURL(r);
+                this.pdf = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+              }
+            )
+          } else {
+            this.isGenerate = false;
+            this.pdf = null;
+          }
+        });
+      }
+      else{
+        this.errormessage = "Merci de sélectionner une classe et éventuellement un groupe avant de générer un QCM";
+      }
       this.isGenerate = false;
+    }
+  }
+
+  onClasseChange($event: MatSelectChange) {
+    for(let i = 0 ; i < this.classes.length ; i++ ){
+      if(this.classes[i].nomClasse == $event.value){
+        this.selectedClasse = this.classes[i];
+      }
+    }
+
+    this.groupesList = []
+    if(this.selectedClasse != undefined){
+      this.groupeService.getGroupesFromUser(this.id,this.selectedClasse.id).subscribe(r => {
+
+        this.groupes = r;
+        for(let i = 0 ; i < r.length ; i++){
+          this.groupesList.push(r[i].nomGroupe)
+        }
+      })
+    }
+  }
+
+  onGroupeChange($event: MatSelectChange) {
+    for(let i = 0 ; i<this.groupes.length ; i++){
+      if(this.groupes[i].nomGroupe == $event.value){
+        this.selectedGroupe = this.groupes[i];
+      }
     }
   }
 }
